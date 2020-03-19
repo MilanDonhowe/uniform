25a041ed5d4480d64cbb7d3f54c2d6d6d85bcbdc
Milan Donhowe <milanaugust@gmail.com>
2020-03-19 14:20:22 -0700

    Updates

---
+++ precache-manifest.0eaa7dab002f606f8845acbc6b57c0d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77713429ae6bcf2069d214ee6e2984f",
+    "url": "/uniform/index.html"
+  },
+  {
+    "revision": "2759ec01a3da1b4381e9",
+    "url": "/uniform/static/css/main.0fb5f51c.chunk.css"
+  },
+  {
+    "revision": "f59b087bea5c15f9b6ab",
+    "url": "/uniform/static/js/2.d11f9678.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/uniform/static/js/2.d11f9678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2759ec01a3da1b4381e9",
+    "url": "/uniform/static/js/main.df83c6f8.chunk.js"
+  },
+  {
+    "revision": "17c1c771039e9a2d4b48",
+    "url": "/uniform/static/js/runtime-main.f5eaf302.js"
+  }
+]);
